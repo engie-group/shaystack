5b63f21d9feecdd71c700273a7c4b051df087f5b
Stuart Longland <stuartl@vrt.com.au>
2016-01-07 13:54:25 +1000

    Add top-level __init__ to package.

---
+++ src/hszinc/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Zinc dumping and parsing module
+# (C) 2016 VRT Systems
+#
+# vim: set ts=4 sts=4 et tw=78 sw=4 si:
+
+from grid import Grid
+from dumper import dump
+from parser import parse
+from metadata import Item, ItemPair, MetadataObject
+from datatypes import Quantity, Coordinate, Uri, Bin, MARKER, Ref
+
+__all__ = ['Grid', 'dump', 'parse', 'Item', 'ItemPair', 'MetadataObject',
+        'Quantity', 'Coordinate', 'Uri', 'Bin', 'MARKER', 'Ref']
+
+__author__ = 'VRT Systems'
+__copyright__ = 'Copyright 2016, VRT Systems'
+__credits__ = ['VRT Systems']
+__license__ = 'BSD'
+__version__ = '0.0.1'
+__maintainer__ = 'VRT Systems'
+__email__ = 'support@vrt.com.au'

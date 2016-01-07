69e794400386e9438567f3a9867527b6c396dc43
Stuart Longland <stuartl@vrt.com.au>
2016-01-07 10:05:24 +1000

    datatypes: Add co-ordinate type

--- src/hszinc/datatypes.py
+++ src/hszinc/datatypes.py
@@ -22,3 +22,22 @@ class Quantity(object):
         return '%s %s' % (
                 self.value, self.unit
         )
+
+
+class Coordinate(object):
+    '''
+    A 2D co-ordinate in degrees latitude and longitude.
+    '''
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __repr__(self):
+        return '%s(%r, %r)' % (
+                self.__class__.__name__, self.latitude, self.longitude
+        )
+
+    def __str__(self):
+        return '%f° lat %f° long' % (
+                self.latitude, self.longitude
+        )

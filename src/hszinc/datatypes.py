9b3f5716411d54c20e63e5ec9c9de18c875b278e
Stuart Longland <stuartl@vrt.com.au>
2016-01-07 11:11:46 +1000

    datatypes: Add in more types from Zinc docs
    There's some ambiguity in the documentation, so I've taken a guess and
    will come back later to refine it.

--- src/hszinc/datatypes.py
+++ src/hszinc/datatypes.py
@@ -41,3 +41,51 @@ class Coordinate(object):
         return '%f° lat %f° long' % (
                 self.latitude, self.longitude
         )
+
+
+class Uri(str):
+    '''
+    A convenience class to allow identification of a URI from other string
+    types.
+    '''
+    pass
+
+
+class Bin(str):
+    '''
+    A convenience class to allow identification of a Bin from other string
+    types.
+    '''
+    # TODO: This seems to be the MIME type, no idea where the data lives.
+    pass
+
+class MarkerType(object):
+    '''
+    A singleton class representing a Marker.
+    '''
+    pass
+
+marker = MarkerType()
+
+
+class Ref(object):
+    '''
+    A reference to an object in Project Haystack.
+    '''
+    # TODO: The grammar specifies that it can have a string following a space,
+    # but the documentation does not specify what this string encodes.  This is
+    # distinct from the reference name itself immediately following the @
+    # symbol.  I'm guessing it's some kind of value.
+    def __init__(self, name, value):
+        self.name   = name
+        self.value  = value
+
+    def __repr__(self):
+        return '%s(%r, %r)' % (
+                self.__class__.__name__, self.name, self.value
+        )
+
+    def __str__(self):
+        return '@%s %r' % (
+                self.name, self.value
+        )

8daa75d65b781bae33b2340decf2c5ffd0db7769
Stuart Longland <stuartl@vrt.com.au>
2016-01-12 13:45:08 +1000

    Re-locate package files.

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 from setuptools import setup
 import sys
-sys.path.insert(0, 'src')
 from hszinc import __version__
 
 setup (name = 'hszinc',
-	package_dir = {'': 'src'},
         version = __version__,
 	packages = [
             'hszinc',

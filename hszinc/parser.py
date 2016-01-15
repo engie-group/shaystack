b77460345bacd28b8aa5cc2a64e6c90d13da36af
Stuart Longland <stuartl@vrt.com.au>
2016-01-15 11:25:45 +1000

    parser: Add parsing of JSON data.

--- hszinc/parser.py
+++ hszinc/parser.py
@@ -16,53 +16,109 @@ import datetime
 import iso8601
 import re
 import six
+import functools
 
 URI_META = re.compile(r'\\([:/\?#\[\]@\\&=;"$`])')
 GRID_SEP = re.compile(r'\n\n+')
 
-def parse(zinc_str):
+MODE_ZINC = 'zinc'
+MODE_JSON = 'json'
+
+# Type regular expressions
+MARKER_STR  = 'm:'
+NUMBER_RE   = re.compile(r'^n:(\d+(:?\.\d+)?(:?[eE][+\-]?\d+)?)(:? (.*))?$')
+REF_RE      = re.compile(r'^r:([a-zA-Z0-9_:\-.~]+)(:? (.*))?$')
+STR_RE      = re.compile(r'^s:(.*)$')
+DATE_RE     = re.compile(r'^d:(\d{4})-(\d{2})-(\d{2})$')
+TIME_RE     = re.compile(r'^h:(\d{2}):(\d{2})(:?:(\d{2}(:?\.\d+)?))$')
+DATETIME_RE = re.compile(r'^t:(\d{4}-\d{2}-\d{2}T'\
+        r'\d{2}:\d{2}(:?:\d{2}(:?\.\d+)?)'\
+        r'(:?[zZ]|[+\-]\d+:?\d*))(:? ([A-Za-z\-_0-9]+))?$')
+URI_RE      = re.compile(r'u:(.+)$')
+BIN_RE      = re.compile(r'b:(.+)$')
+COORD_RE    = re.compile(r'c:(\d+\.?\d*),(\d+\.?\d*)$')
+
+def parse(grid_str, mode=MODE_ZINC):
     '''
     Parse the given Zinc text and return the equivalent data.
     '''
     # Split the separate grids up, the grammar definition has trouble splitting
     # them up normally.  This will truncate the newline off the end of the last
     # row.
-    return list(map(parse_grid, GRID_SEP.split(zinc_str.rstrip())))
-
-def parse_grid(grid_str):
-    if not grid_str.endswith('\n'):
-        grid_str += '\n'
-
-    parsed = zinc_grammar.parse(grid_str)
-    assert len(parsed.children) == 3
-
-    # First child is metadata
-    meta = parsed.children[0]
-    assert meta.expr_name == 'gridMeta'
-
-    # We expect the version string next
-    assert meta.children[0].expr_name == 'ver'
-    version = parse_str(meta.children[0].children[1])
-
-    # The metadata should be in a nameless node.  Parse the children.
-    if bool(meta.children[1].children):
-        meta_items = parse_meta(meta.children[1].children[0])
+    _parse = functools.partial(parse_grid, mode=mode)
+    if (mode == MODE_JSON) and isinstance(grid_str, six.string_types):
+        return list(map(_parse, json.loads(grid_str)))
     else:
-        # No metadata
-        meta_items = MetadataObject()
+        return list(map(_parse, GRID_SEP.split(grid_str.rstrip())))
 
-    # The last thing in the list should be a newline
-    assert meta.children[2].expr_name == 'nl'
+def parse_grid(grid_str, mode=MODE_ZINC):
+    if mode == MODE_ZINC:
+        if not grid_str.endswith('\n'):
+            grid_str += '\n'
 
-    # The next item should be the columns
-    columns = parse_columns(parsed.children[1])
+        parsed = zinc_grammar.parse(grid_str)
+        assert len(parsed.children) == 3
 
-    # This is enough to construct the base grid.
-    grid = Grid(version=version, metadata=meta_items, columns=columns)
+        # First child is metadata
+        meta = parsed.children[0]
+        assert meta.expr_name == 'gridMeta'
 
-    # Now to parse the rows
-    if bool(columns):
-        parse_rows(grid, parsed.children[2])
+        # We expect the version string next
+        assert meta.children[0].expr_name == 'ver'
+        version = parse_str(meta.children[0].children[1])
+
+        # The metadata should be in a nameless node.  Parse the children.
+        if bool(meta.children[1].children):
+            meta_items = parse_meta(meta.children[1].children[0])
+        else:
+            # No metadata
+            meta_items = MetadataObject()
+
+        # The last thing in the list should be a newline
+        assert meta.children[2].expr_name == 'nl'
+
+        # The next item should be the columns
+        columns = parse_columns(parsed.children[1])
+
+        # This is enough to construct the base grid.
+        grid = Grid(version=version, metadata=meta_items, columns=columns)
+
+        # Now to parse the rows
+        if bool(columns):
+            parse_rows(grid, parsed.children[2])
+    elif mode == MODE_JSON:
+        # Grab the metadata
+        if isinstance(grid_str, six.string_types):
+            parsed = json.loads(grid_str)
+        else:
+            parsed = grid_str
+        meta = parsed.pop('meta')
+        version = meta.pop('ver')
+
+        # Parse the remaining elements
+        metadata = {}
+        for name, value in meta.items():
+            metadata[name] = parse_scalar(value, mode=mode)
+
+        grid = Grid(version=version, metadata=metadata)
+
+        # Grab the columns in the order given
+        for col in parsed.pop('cols'):
+            name = col.pop('name')
+            meta = {}
+            for key, value in col.items():
+                meta[key] = parse_scalar(value, mode=mode)
+            grid.column[col] = meta
+
+        # Parse the rows
+        for row in (parsed.pop('rows',[]) or []):
+            parsed_row = {}
+            for col, value in row.items():
+                parsed_row[col] = parse_scalar(value, mode=mode)
+            grid.append(parsed_row)
+    else:
+        raise NotImplementedError('Format not implemented: %s' % mode)
+
     return grid
 
 def parse_meta(meta):
@@ -166,40 +222,131 @@ def parse_cell(cell):
     else:
         return None
 
-def parse_scalar(scalar):
-    # Should have a child which is the type of scalar.
-    assert scalar.expr_name == 'scalar'
-    assert len(scalar.children) == 1
-    scalar_child = scalar.children[0]
-    if scalar_child.expr_name == 'null':
-        return None
-    elif scalar_child.expr_name == 'marker':
-        return MARKER
-    elif scalar_child.expr_name == 'remove':
-        return REMOVE
-    elif scalar_child.expr_name == 'bool':
-        return parse_bool(scalar_child)
-    elif scalar_child.expr_name == 'ref':
-        return parse_ref(scalar_child)
-    elif scalar_child.expr_name == 'bin':
-        return parse_bin(scalar_child)
-    elif scalar_child.expr_name == 'str':
-        return parse_str(scalar_child)
-    elif scalar_child.expr_name == 'uri':
-        return parse_uri(scalar_child)
-    elif scalar_child.expr_name == 'date':
-        return parse_date(scalar_child)
-    elif scalar_child.expr_name == 'time':
-        return parse_time(scalar_child)
-    elif scalar_child.expr_name == 'dateTime':
-        return parse_date_time(scalar_child)
-    elif scalar_child.expr_name == 'coord':
-        return parse_coord(scalar_child)
-    elif scalar_child.expr_name == 'number':
-        return parse_number(scalar_child)
-    else: # pragma: no cover
-        raise NotImplementedError('Unhandled case: %s' \
-                % scalar_child.prettily())
+def parse_scalar(scalar, mode=MODE_ZINC):
+    if mode == MODE_ZINC:
+        # Should have a child which is the type of scalar.
+        assert scalar.expr_name == 'scalar'
+        assert len(scalar.children) == 1
+        scalar_child = scalar.children[0]
+        if scalar_child.expr_name == 'null':
+            return None
+        elif scalar_child.expr_name == 'marker':
+            return MARKER
+        elif scalar_child.expr_name == 'remove':
+            return REMOVE
+        elif scalar_child.expr_name == 'bool':
+            return parse_bool(scalar_child)
+        elif scalar_child.expr_name == 'ref':
+            return parse_ref(scalar_child)
+        elif scalar_child.expr_name == 'bin':
+            return parse_bin(scalar_child)
+        elif scalar_child.expr_name == 'str':
+            return parse_str(scalar_child)
+        elif scalar_child.expr_name == 'uri':
+            return parse_uri(scalar_child)
+        elif scalar_child.expr_name == 'date':
+            return parse_date(scalar_child)
+        elif scalar_child.expr_name == 'time':
+            return parse_time(scalar_child)
+        elif scalar_child.expr_name == 'dateTime':
+            return parse_date_time(scalar_child)
+        elif scalar_child.expr_name == 'coord':
+            return parse_coord(scalar_child)
+        elif scalar_child.expr_name == 'number':
+            return parse_number(scalar_child)
+        else: # pragma: no cover
+            raise NotImplementedError('Unhandled case: %s' \
+                    % scalar_child.prettily())
+    elif mode == MODE_JSON:
+        # Simple cases
+        if scalar is None:
+            return None
+        elif scalar == MARKER_STR:
+            return MARKER
+
+        # Is it a number?
+        match = NUMBER_RE.match(scalar)
+        if match:
+            # We'll get a value and a unit, amongst other tokens.
+            matched = match.groups()
+            value = float(matched[0])
+            if matched[-1] is not None:
+                # It's a quantity
+                return Quantity(value, matched[-1])
+            else:
+                # It's a raw value
+                return value
+
+        # Is it a string?
+        match = STR_RE.match(scalar)
+        if match:
+            return match.group(1)
+
+        # Is it a reference?
+        match = REF_RE.match(scalar)
+        if match:
+            matched = match.groups()
+            if matched[-1] is not None:
+                return Ref(matched[0], matched[-1], has_value=True)
+            else:
+                return Ref(matched[0])
+
+        # Is it a date?
+        match = DATE_RE.match(scalar)
+        if match:
+            (year, month, day) = match.groups()
+            return datetime.date(year=int(year), month=int(month), day=int(day))
+
+        # Is it a time?
+        match = TIME_RE.match(scalar)
+        if match:
+            (hour, minute, second) = match.groups()
+            # Convert second to seconds and microseconds
+            if '.' in second:
+                (whole_sec, frac_sec) = second.split('.',1)
+                sec = int(whole_sec)
+                usec = int(frac_sec[:6].ljust(6,'0'))
+            else:
+                sec = int(second)
+                usec = 0
+            return datetime.time(hour=int(hour), minute=int(minute),
+                    second=sec, microseconds=usec)
+
+        # Is it a date/time?
+        match = DATETIME_RE.match(scalar)
+        if match:
+            matches = match.groups()
+            # Parse ISO8601 component
+            isodate = iso8601.parse_date(matches[0])
+            # Parse timezone
+            tzname  = matches[-1]
+            if tzname is None:
+                return isodate  # No timezone given
+            else:
+                try:
+                    tz = timezone(tzname)
+                    return isodate.astimezone(tz)
+                except:
+                    return isodate
+
+        # Is it a URI?
+        match = URI_RE.match(scalar)
+        if match:
+            return Uri(match.group(1))
+
+        # Is it a Bin?
+        match = BIN_RE.match(scalar)
+        if match:
+            return Bin(match.group(1))
+
+        # Is it a co-ordinate?
+        match = COORD_RE.match(scalar)
+        if match:
+            (lat,lng) = match.groups()
+            return Coord(float(lat),float(lng))
+        return scalar
+    else:
+        raise NotImplementedError('Format not implemented: %s' % mode)
 
 def parse_id(id_node):
     assert id_node.expr_name == 'id'

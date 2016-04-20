11512683affdd2d684f044fc1f4f0e8aaf264952
Stuart Longland <stuartl@vrt.com.au>
2016-04-21 07:38:38 +1000

    Release 0.0.5
    - Support for character sets (credit Christian Tremblay)
    - String test case fixes for Python 2.
    - Copy operators for the REMOVE and MARKER singletons.
    - Pretty-printing __repr__ operator for grids.
    - Tweak package requirements specs.

--- hszinc/__init__.py
+++ hszinc/__init__.py
@@ -18,6 +18,6 @@ __author__ = 'VRT Systems'
 __copyright__ = 'Copyright 2016, VRT Systems'
 __credits__ = ['VRT Systems']
 __license__ = 'BSD'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 __maintainer__ = 'VRT Systems'
 __email__ = 'support@vrt.com.au'

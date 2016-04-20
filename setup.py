f0fd619d12722f3299fd807c95d880588e45aa0d
Stuart Longland <stuartl@vrt.com.au>
2016-04-21 07:38:07 +1000

    setup.py: Add package requirements.
    Not everything looks at requirements.txt.

--- setup.py
+++ setup.py
@@ -8,4 +8,10 @@ setup (name = 'hszinc',
 	packages = [
             'hszinc',
         ],
+        requires=[
+            'parsimonious',
+            'pytz',
+            'iso8601',
+            'six',
+        ]
 )

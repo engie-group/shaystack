6cd4b0255a2388d652028ad23f2a63325034a242
Stuart Longland <stuartl@vrt.com.au>
2016-04-30 08:04:06 +1000

    __init__: Catch ImportError so setup.py works.
    setup.py crashes if the dependencies aren't there, so requirements and
    version information cannot be interrogated correctly.  This catches the
    error so that setup.py works.

--- hszinc/__init__.py
+++ hszinc/__init__.py
@@ -4,15 +4,20 @@
 #
 # vim: set ts=4 sts=4 et tw=78 sw=4 si:
 
-from .grid import Grid
-from .dumper import dump, dump_scalar
-from .parser import parse, MODE_JSON, MODE_ZINC
-from .metadata import MetadataObject
-from .datatypes import Quantity, Coordinate, Uri, Bin, MARKER, REMOVE, Ref
+try:
+    from .grid import Grid
+    from .dumper import dump, dump_scalar
+    from .parser import parse, MODE_JSON, MODE_ZINC
+    from .metadata import MetadataObject
+    from .datatypes import Quantity, Coordinate, Uri, Bin, MARKER, REMOVE, Ref
 
-__all__ = ['Grid', 'dump', 'parse', 'MetadataObject', 'Quantity',
-        'Coordinate', 'Uri', 'Bin', 'MARKER', 'REMOVE', 'Ref',
-        'MODE_JSON', 'MODE_ZINC']
+    __all__ = ['Grid', 'dump', 'parse', 'MetadataObject', 'Quantity',
+            'Coordinate', 'Uri', 'Bin', 'MARKER', 'REMOVE', 'Ref',
+            'MODE_JSON', 'MODE_ZINC']
+except ImportError: # pragma: no cover
+    # For setup.py to interrogate the version information.  This should *NOT*
+    # get executed in production, and if it did, things wouldn't work anyway.
+    pass
 
 __author__ = 'VRT Systems'
 __copyright__ = 'Copyright 2016, VRT Systems'

923cca72d19140f417054533ddcfaa71297892ef
Stuart Longland <stuartl@vrt.com.au>
2016-07-30 13:14:10 +1000

    tests.test_datatype: Add checks for Ref != operator

--- tests/test_datatype.py
+++ tests/test_datatype.py
@@ -30,14 +30,48 @@ def test_marker_copy():
 def test_remove_copy():
     check_singleton_copy(hszinc.REMOVE)
 
+def test_ref_notref_eq():
+    r1 = hszinc.Ref(name='a.ref')
+    r2 = 'not.a.ref'
+    assert r1 is not r2
+    try:
+        if r1 != r2:
+            assert False, 'Somehow compared ref to string'
+    except TypeError:
+        pass
+
 def test_ref_simple_eq():
     r1 = hszinc.Ref(name='a.ref')
     r2 = hszinc.Ref(name='a.ref')
     assert r1 is not r2
     assert r1 == r2
 
-def test_ref_simple_neq():
+def test_ref_simple_neq_id():
     r1 = hszinc.Ref(name='a.ref')
     r2 = hszinc.Ref(name='another.ref')
+    assert r1 is not r2
+    assert r1 != r2
+
+def test_ref_mixed_neq():
+    r1 = hszinc.Ref(name='a.ref')
+    r2 = hszinc.Ref(name='a.ref', value='display text')
+    assert r1 is not r2
+    assert r1 != r2
+
+def test_ref_withdis_eq():
+    r1 = hszinc.Ref(name='a.ref', value='display text')
+    r2 = hszinc.Ref(name='a.ref', value='display text')
+    assert r1 is not r2
+    assert r1 == r2
+
+def test_ref_withdis_neq_id():
+    r1 = hszinc.Ref(name='a.ref', value='display text')
+    r2 = hszinc.Ref(name='another.ref', value='display text')
+    assert r1 is not r2
+    assert r1 != r2
+
+def test_ref_withdis_neq_dis():
+    r1 = hszinc.Ref(name='a.ref', value='display text')
+    r2 = hszinc.Ref(name='a.ref', value='different display text')
     assert r1 is not r2
     assert r1 != r2

37d6846208cb3f53f276930d47e76d22fbf196df
Stuart Longland <stuartl@vrt.com.au>
2018-02-12 22:16:39 +1000

    WC-1297: version: Move version detection logic.

--- hszinc/version.py
+++ hszinc/version.py
@@ -27,6 +27,7 @@ So, in this implementation:
 """
 
 import re
+import warnings
 
 VERSION_RE = re.compile(r'^(\d[\d\.]*)([^\d].*)*$')
 
@@ -34,6 +35,7 @@ class Version(object):
     """
     A Project Haystack version number
     """
+
     def __init__(self, ver_str):
         if isinstance(ver_str, Version):
             # Clone constructor
@@ -121,8 +123,57 @@ class Version(object):
     def __gt__(self, other):
         return self._cmp(other) > 0
 
+    @classmethod
+    def nearest(self, ver):
+        """
+        Retrieve the official version nearest the one given.
+        """
+        if not isinstance(ver, Version):
+            ver = Version(ver)
+
+        if ver in OFFICIAL_VERSIONS:
+            return ver
+
+        # We might not have an exact match for that.
+        # See if we have one that's newer than the grid we're looking at.
+        versions = list(OFFICIAL_VERSIONS)
+        versions.sort(reverse=True)
+        best = None
+        for candidate in versions:
+            # Due to ambiguities, we might have an exact match and not know it.
+            # '2.0' will not hash to the same value as '2.0.0', but both are
+            # equivalent.
+            if candidate == ver:
+                # We can't beat this, make a note of the match for later
+                return candidate
+
+            # If we have not seen a better candidate, and this is older
+            # then we may have to settle for that.
+            if (best is None) and (candidate < ver):
+                warnings.warn('This version of hszinc does not yet '\
+                            'support version %s, please seek a newer version '\
+                            'or file a bug.  Closest (older) version supported is %s.'\
+                            % (ver, candidate))
+                return candidate
+
+            # Probably the best so far, but see if we can go closer
+            if candidate > ver:
+                best = candidate
+
+        # Unhappy path, no best option?  This should not happen.
+        assert best is not None
+        warnings.warn('This version of hszinc does not yet '\
+                    'support version %s, please seek a newer version '\
+                    'or file a bug.  Closest (newer) version supported is %s.'\
+                    % (ver, best))
+        return best
+
 
 # Standard Project Haystack versions
 VER_2_0 = Version('2.0')
 VER_3_0 = Version('3.0')
 LATEST_VER = VER_3_0
+
+OFFICIAL_VERSIONS = set([
+    VER_2_0, VER_3_0
+])

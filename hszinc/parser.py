0738a9d10d01c4dadaf3b0750430780be2db5519
Stuart Longland <stuartl@vrt.com.au>
2020-09-01 20:34:02 +1000

    WC-3135: parser: Handle 'zinc' and 'json' as modes.
    Some code ignores the constants set in `parser.py` and just supplies its
    own string (a lot of VRT-internal stuff does this out of laziness).  To
    avoid lots of mass breakage, let's handle this as gracefully as
    possible.

--- hszinc/parser.py
+++ hszinc/parser.py
@@ -22,11 +22,34 @@ GRID_SEP = re.compile(r'(?<=\n)\n+')
 MODE_ZINC = 'text/zinc'
 MODE_JSON = 'application/json'
 
+def _parse_mode(mode):
+    """
+    Sanitise the mode given.  Whilst code _should_ use the MODE_ZINC and
+    MODE_JSON constants above, we have some code internally that plays fast
+    and loose with this.
+    """
+    if mode in (MODE_ZINC, MODE_JSON):
+        return mode
+
+    # Danger zone: user has given us something different.  They better know
+    # what it is they are doing!
+    mode = str(mode).lower()
+    if mode == 'zinc':
+        return MODE_ZINC
+    elif mode == 'json':
+        return MODE_JSON
+    else:
+        # Clearly that was a wrong assumption.  Let 'em have it!
+        raise ValueError('Unrecognised mode, should be MODE_ZINC or MODE_JSON')
+
 
 def parse(grid_str, mode=MODE_ZINC, charset='utf-8', single=True):
     """
     Parse the given Zinc text and return the equivalent data.
     """
+    # Sanitise mode
+    mode = _parse_mode(mode)
+
     # Decode incoming text (or python3 will whine!)
     if isinstance(grid_str, six.binary_type):
         grid_str = grid_str.decode(encoding=charset)
@@ -61,6 +84,9 @@ def parse(grid_str, mode=MODE_ZINC, charset='utf-8', single=True):
 
 
 def parse_grid(grid_str, mode=MODE_ZINC, charset='utf-8'):
+    # Sanitise mode
+    mode = _parse_mode(mode)
+
     # Decode incoming text
     if isinstance(grid_str, six.binary_type):  # pragma: no cover
         # No coverage here, because it *should* be handled above unless the user
@@ -76,6 +102,9 @@ def parse_grid(grid_str, mode=MODE_ZINC, charset='utf-8'):
 
 
 def parse_scalar(scalar, mode=MODE_ZINC, version=LATEST_VER, charset='utf-8'):
+    # Sanitise mode
+    mode = _parse_mode(mode)
+
     # Decode version string
     if not isinstance(version, Version):
         version = Version(version)

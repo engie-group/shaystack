36bd5be12d0fc9ecbbf1513d6a841b2e570d8384
Philippe PRADOS <github@prados.fr>
2020-10-21 16:58:54 +0200

    - Generate GraphQL Schema from Haystack schema - Some field clash with Python at this time

--- tests/tst_graphql.py
+++ tests/tst_graphql.py
@@ -1,57 +1,199 @@
 import logging
 import os
 from datetime import datetime
+from functools import partial
+from pathlib import Path
 from typing import Optional, List
 
 import graphene
-from graphene import Scalar
+from graphene import Scalar, Node
 from graphql.language import ast
-from graphql.utils import schema_printer
 
 import hszinc
 from haystackapi.providers.haystack_interface import get_singleton_provider
-from hszinc import Ref
+from hszinc import Ref, MODE_JSON
 
 log = logging.getLogger("haystackapi")
 log.setLevel(level=logging.getLevelName(os.environ.get("LOGLEVEL", "WARNING")))
 
-class HSRef(Scalar):
-    @staticmethod
-    def serialize(hs_scalar):
-        return hszinc.dump_scalar(hs_scalar, hszinc.MODE_JSON, version=hszinc.VER_3_0)
 
-    @staticmethod
+# L'idée est d'avoir des valeurs avec typage, pour éviter les ambiguités
+# C'est au client d'en tenir compte.
+
+# Une autre idée est de produire un type "compatible" GraphQL/Json, mais on perd de l'info.
+
+class _HSKind(Scalar):
+    serialize = partial(hszinc.dump_scalar, mode=hszinc.MODE_JSON, version=hszinc.VER_3_0)
+
     # Parse from AST See https://tinyurl.com/y3fr76a4
-    def parse_literal(node):
+    def parse_literal(node: Node):
         if isinstance(node, ast.StringValue):  # FIXME
-            return f"node={node}"
+            return hszinc.dump_scalar(ast.value, mode=hszinc.MODE_JSON, version=hszinc.VER_3_0)
 
-    @staticmethod
     # Parse form json
+    # FIXME parse_value = partial(hszinc.dump_scalar, mode=hszinc.MODE_JSON, version=hszinc.VER_3_0)
     def parse_value(value):
         return f"parse_value={value}"  # FIXME
 
 
-class HSMarker(Scalar):
-    @staticmethod
-    def serialize(hs_scalar):
-        return hszinc.dump_scalar(hs_scalar, hszinc.MODE_JSON, version=hszinc.VER_3_0)
+class HSMarker(_HSKind):
+    class Meta:
+        description = "Haystack marker"
 
-    @staticmethod
-    # Parse from AST See https://tinyurl.com/y3fr76a4
-    def parse_literal(node):
-        if isinstance(node, ast.StringValue):  # FIXME
-            return f"node={node}"
 
-    @staticmethod
-    # Parse form json
-    def parse_value(value):
-        return f"parse_value={value}"  # FIXME
+class HSBool(_HSKind):
+    class Meta:
+        description = "Haystack bool"
 
-class Entity(graphene.ObjectType):
+
+class HSNA(_HSKind):
+    class Meta:
+        description = "Haystack NA"
+
+
+class HSNumber(_HSKind):
+    class Meta:
+        description = "Haystack Number with unit"
+
+
+class HSStr(graphene.String):
+    class Meta:
+        description = "Haystack str"
+
+
+class HSUri(_HSKind):
+    class Meta:
+        description = "Haystack uri"
+
+
+class HSRef(_HSKind):
+    class Meta:
+        description = "Haystack reference"
+
+
+class HSBin(_HSKind):
+    class Meta:
+        description = "Haystack bin"
+
+
+class HSDate(_HSKind):
+    class Meta:
+        description = "Haystack date"
+
+
+class HSTime(_HSKind):
+    class Meta:
+        description = "Haystack time"
+
+
+class HSDateTime(_HSKind):
+    class Meta:
+        description = "Haystack DateTime"
+
+
+class HSCoord(_HSKind):
+    class Meta:
+        description = "Haystack Coord"
+
+
+class HSXStr(_HSKind):
+    class Meta:
+        description = "Haystack XStr"
+
+
+class HSList(graphene.JSONString):
+    class Meta:
+        description = "Haystack List"
+
+
+class HSDict(graphene.JSONString):
+    class Meta:
+        description = "Haystack dict"
+
+
+class HSGrid(graphene.JSONString):
+    class Meta:
+        description = "Haystack grid"
+
+
+class XEntity(graphene.ObjectType):
     id = graphene.Field(HSRef)
     site = graphene.Field(HSMarker)
     dis = graphene.String()
+    toto = graphene.Field(_HSKind)
+
+
+def _hskind_to_grafene(kind: str) -> str:
+    kind = kind.lower()
+    if kind == "marker":
+        return "graphene.Field(HSMarker)"
+    if kind == "bool":
+        return "graphene.Field(HSBool)"
+    if kind == "na":
+        return "graphene.Field(HSNA)"
+    if kind == "number":
+        return "graphene.Field(HSNumber)"
+    if kind == "str":
+        return "graphene.String()"
+    if kind == "uri":
+        return "graphene.Field(HSUri)"
+    if kind == "ref":
+        return "graphene.Field(HSRef)"
+    if kind == "bin":
+        return "graphene.Field(HSBin)"
+    if kind == "date":
+        return "graphene.Field(HSDate)"
+    if kind == "time":
+        return "graphene.Field(HSTime)"
+    if kind == "datetime":
+        return "graphene.Field(HSDateTime)"
+    if kind == "coord":
+        return "graphene.Field(HSCoord)"
+    if kind == "xstr":
+        return "graphene.Field(HSXStr)"
+    if kind == "list":
+        return "graphene.Field(HSList)"
+    if kind == "dict":
+        return "graphene.Field(HSDict)"
+    if kind == "grid":
+        return "graphene.Field(HSGrid)"
+    if kind == "obj":
+        return "graphene.Field(_HSKind)"
+    assert False, f"Invalid {kind}"
+
+
+# FIXME: voir import
+def reserve_word(col: str) -> str:
+    if col in ("import", "def", "if"):  # FIXME
+        col = col + "_"
+
+
+def generate_entity_from_schema(filename: Path):
+    with open(filename) as f:
+        schema = hszinc.parse(f.read(), MODE_JSON)
+    body = [f"\t{col} = {_hskind_to_grafene(meta['kind'])}" for col, meta in schema.column.items()]
+    entity_source_code = "class Entity(graphene.ObjectType):\n" + "\n".join(body)
+    # print(entity_source_code)
+    exec(entity_source_code, globals(), locals())
+    return locals()['Entity']
+
+
+Entity = generate_entity_from_schema(Path("tests/marriott_schema.json"))
+
+
+# FIXME Entity = generate_entity_from_schema(Path("tests/haystack_schema.json"))
+
+class About(graphene.ObjectType):
+    haystackVersion = graphene.String()
+    tz = graphene.String()
+    serverName = graphene.String()
+    serverTime = graphene.Field(HSTime)
+    serverBootTime = graphene.Field(HSTime)
+    productName = graphene.String()
+    productUri = graphene.Field(HSUri)
+    productVersion = graphene.String()
+    moduleName = graphene.String()
+    moduleVersion = graphene.String()
 
 
 class ReadHaystack(graphene.ObjectType):
@@ -59,21 +201,31 @@ class ReadHaystack(graphene.ObjectType):
         description = 'Ontology conform with Haystack project'
         name = "Haystack"
 
+    about = graphene.List(
+        graphene.NonNull(About),
+    )
+
     read = graphene.List(
         graphene.NonNull(Entity),
         ids=graphene.List(graphene.ID),
-        select=graphene.String(default_value='*'),
         filter=graphene.String(default_value=''),
         limit=graphene.Int(default_value=0),
         version=graphene.DateTime()
     )
 
-    # @staticmethod
-    # def resolve_about(parent, info):
-    #     log.debug(f"resolve_about(parent,info)")
-    #     grid = get_singleton_provider().about("dev")  # FIXME: dev
-    #     return [[HSTag(name, val) for name, val in entity.items()] for entity in grid]
-    #
+    @staticmethod
+    def resolve_about(parent, info):
+        log.debug(f"resolve_about(parent,info)")
+        grid = get_singleton_provider().about("dev")  # FIXME: dev
+        result = []
+        for row in grid:
+            about = About()
+            for key, val in row.items():
+                if key in about.__dict__:
+                    about.__setattr__(key, val)
+            result.append(about)
+        return result
+
     # @staticmethod
     # def resolve_ops(parent, info):
     #     log.debug(f"resolve_about(parent,info)")
@@ -83,18 +235,27 @@ class ReadHaystack(graphene.ObjectType):
     @staticmethod
     def resolve_read(parent, info,
                      ids: Optional[List[str]] = None,
-                     select: str = '*',
                      filter: str = '',
                      limit: int = 0,
                      version: Optional[datetime] = None):
-        log.debug(f"resolve_haystack(parent,info,ids={ids}, query={filter}, limit={limit}, datetime={datetime})")
+        log.debug(f"resolve_haystack(parent,info,ids={ids}, filter={filter}, limit={limit}, datetime={datetime})")
+        # Extraction des fields à récuperer
+        field_selected = [field.name.value for field in info.field_asts[0].selection_set.selections]
+        # FIXME: modifier type de select pour avoir directement un liste ?
+        select = ",".join(field_selected)
         if ids:
             ids = [Ref(ReadHaystack._filter_id(id)) for id in ids]
         grid = get_singleton_provider().read(limit, select, ids, filter, version)
-        entity = Entity()
-        entity.dis = "hello"
-        # return [Entity([HSTag(name, val) for name, val in entity.items()] for entity in grid]
-        return [entity]
+        # FIXME: normalement Entity(**grid[0])
+        # return [ Entity(*row) for row in grid]
+        result = []
+        for row in grid:
+            entity = Entity()
+            for key, val in row.items():
+                if key in entity.__dict__:
+                    entity.__setattr__(key, val)
+            result.append(entity)
+        return result
 
     # @staticmethod
     # def resolve_his_read(parent, info,
@@ -127,7 +288,6 @@ class ReadHaystack(graphene.ObjectType):
 
 
 # TODO: WriteHaystack avec point_write_write et his_write
-
 class Query(graphene.ObjectType):
     class Meta:
         description = 'Top query'
@@ -140,27 +300,41 @@ class Query(graphene.ObjectType):
 
 
 hs_schema = graphene.Schema(query=Query)
+
+
+def dump_graphql_schema():
+    # Print only haystack schema
+    from graphql.utils import schema_printer
+    print(schema_printer.print_schema(graphene.Schema(query=ReadHaystack)))
+
+
 result = hs_schema.execute('''
 { 
     # haystack(select: "id,dis"ids: ["@customer-913"])
     haystack 
     {
-        # about 
-        # {
-        #     name
-        #     value
-        # }
+        about {
+            haystackVersion
+            tz
+            serverName
+            serverTime
+            serverBootTime
+            productName
+            productUri
+            productVersion
+            moduleName
+            moduleVersion
+        }
 #         ops 
 #         {
 #             name
 #             value
 #         }
-#         read(select: "id,dis" filter: "id", limit: 2)
-       read
-        { 
-            site
-            dis
-        }
+#         read(filter: "site", limit: 2)
+#         { 
+#             site
+#             dis
+#         }
 #         hisRead(id:"@elec-16514")
 #         {
 #             name
@@ -174,6 +348,7 @@ result = hs_schema.execute('''
     }
 }
 ''')
-print(schema_printer.print_schema(hs_schema))
+# dump_graphql_schema()
 print("result = " + str(result.data))
 print("error = " + str(result.errors))
+

49cc3f71b617dfd43de0f70280ae9e9f24b7d886
Philippe Prados <pprados@octo.com>
2021-02-11 15:12:17 +0100

    UI for demo

---
+++ app/haystackui/precache-manifest.62d86b92d07512b2d897b0f8f7def431.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3aa27e92a0298c8e032",
+    "url": "/css/app.48a827c1.css"
+  },
+  {
+    "revision": "2c0e9e7855b11919b1b9",
+    "url": "/css/chunk-vendors.d322079d.css"
+  },
+  {
+    "revision": "1993c4a16cce5446a5cfafacf4da740b",
+    "url": "/fonts/materialdesignicons-webfont.1993c4a1.eot"
+  },
+  {
+    "revision": "26333b3884782fdb5f8c0a971f1d2570",
+    "url": "/fonts/materialdesignicons-webfont.26333b38.woff"
+  },
+  {
+    "revision": "af3f6dbe172a9c1aba77366a2ff630d1",
+    "url": "/fonts/materialdesignicons-webfont.af3f6dbe.woff2"
+  },
+  {
+    "revision": "baded94134f273450a473a4962111324",
+    "url": "/fonts/materialdesignicons-webfont.baded941.ttf"
+  },
+  {
+    "revision": "9e9d67b9c02b557996e57dc2f66e724f",
+    "url": "/img/engieLogo.9e9d67b9.png"
+  },
+  {
+    "revision": "032eb679a327d4668d7537c32c314fbc",
+    "url": "/img/materialdesignicons-webfont.032eb679.svg"
+  },
+  {
+    "revision": "17312d457a13546736f2826e62df520d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3aa27e92a0298c8e032",
+    "url": "/js/app.aa8a1680.js"
+  },
+  {
+    "revision": "2c0e9e7855b11919b1b9",
+    "url": "/js/chunk-vendors.b529b837.js"
+  },
+  {
+    "revision": "a9c592c778e8c28a6e92aa34256079c9",
+    "url": "/js/config.js"
+  }
+]);

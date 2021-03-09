5addf70295b9a1d73d80c657cc50b1af7c4b18b5
Philippe Prados <pprados@octo.com>
2021-03-09 16:07:41 +0100

    WIP: manage all but path and without customer_id or versions

--- shaystack/providers/db_mongo.py
+++ shaystack/providers/db_mongo.py
@@ -1,9 +1,87 @@
 from datetime import datetime
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, List, Union
+
+from shaystack import parse_filter, HaystackType, Quantity
+from shaystack.filter_ast import FilterNode, FilterUnary, FilterPath, FilterBinary
+from ..jsondumper import dump_scalar as json_dump_scalar
+
+_simple_ops = {
+    "==": "$eq",
+    "!=": "$ne",
+    "and": "$and",
+    "or": "$or",
+}
+_relative_ops = {
+    "<": "$lt",
+    "<=": "$lte",
+    ">": "$gt",
+    ">=": "$gte",
+}
+
+
+def _to_float(scalar: HaystackType) -> float:
+    if isinstance(scalar, Quantity):
+        return scalar.magnitude
+    if isinstance(scalar, (int, float)):
+        return scalar
+    raise ValueError("Impossible to compare with not a numnber")  # FIXME
+
+
+def _conv_filter(node: Union[FilterNode, HaystackType]) -> Union[Dict[str, Any], str]:
+    if isinstance(node, FilterUnary):
+        if node.operator == "has":
+            return {"$cond": {"if": _conv_filter(node.right), "then": 1, "else": 0}}
+        if node.operator == "not":
+            right_expr = _conv_filter(node.right)
+            if isinstance(node, FilterNode):
+                return {"$not": _conv_filter(node.right)}
+            else:
+                return {"$cond": {"if": _conv_filter(node.right), "then": 0, "else": 1}}
+    if isinstance(node, FilterPath):
+        return '$' + node.paths[0]
+    if isinstance(node, FilterBinary):
+        if node.operator in _simple_ops:
+            return {_simple_ops[node.operator]: [
+                _conv_filter(node.left),
+                _conv_filter(node.right),
+            ]}
+        if node.operator in _relative_ops:
+            path = _conv_filter(node.left)
+            to_double = {
+                "$let": {
+                    "vars": {
+                        f"{path[1:]}_": {
+                            "$regexFind": {
+                                "input": f"{path}",
+                                "regex": "n:([-+]?([0-9]*[.])?[0-9]+([eE][-+]?\\d+)?)"
+                            }
+                        }
+                    },
+                    "in": {"$toDouble": {
+                        "$arrayElemAt": [f"${path}_.captures", 0]
+                    }}
+                }
+            }
+
+            return {_relative_ops[node.operator]: [
+                to_double,
+                _to_float(node.right),
+            ]}
+        assert 0, "Invalid operator"  # FIXME
+    else:
+        return json_dump_scalar(node)
 
 
 def _mongo_filter(grid_filter: Optional[str],
                   version: datetime,
                   limit: int = 0,
-                  customer_id: str = '') -> Dict[str, Any]:
-    return "FIXME"  # FIXME
+                  customer_id: str = '') -> List[Dict[str, Any]]:
+    return [
+        {
+            "$match":
+                {
+                    "$expr": _conv_filter(parse_filter(grid_filter).head)
+                }
+        }
+    ]
+    return
--- tests/test_mongodb.py
+++ tests/test_mongodb.py
@@ -24,252 +24,345 @@ def _check_mongodb(mongo_request: Dict[str, Any]):
         envs = {'HAYSTACK_DB': os.environ['HAYSTACK_DB']}
         provider = cast(MongoProvider, get_provider("shaystack.providers.mongodb", envs))
         collection = provider.get_collection()
-        collection.find(mongo_request)
+        list(collection.aggregate(mongo_request))
 
 
 def test_tag():
     hs_filter = 'site'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$cond': {'if': '$site', 'then': 1, 'else': 0}}}}]
 
 
 def test_not_tag():
     hs_filter = 'not site'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$not': '$site'}}}]
 
 
 def test_equal_ref():
     hs_filter = 'a == @id'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"r:id"']}}}]
 
 
 def test_equal_str():
     hs_filter = 'a == "abc"'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"s:abc"']}}}]
 
 
 def test_equal_int():
     hs_filter = 'a == 1'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"n:1.000000"']}}}]
 
 
 def test_equal_float():
     hs_filter = 'a == 1.0'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"n:1.000000"']}}}]
 
 
 def test_equal_bool():
     hs_filter = 'a == true'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', 'true']}}}]
 
 
 def test_equal_datetime():
     hs_filter = 'a == 1977-04-22T01:00:00-00:00'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"t:1977-04-22T01:00:00+00:00 UTC"']}}}]
 
 
 def test_equal_time():
     hs_filter = 'a == 01:00:00'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"h:01:00:00"']}}}]
 
 
 def test_equal_date():
     hs_filter = 'a == 1977-04-22'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"d:1977-04-22"']}}}]
 
 
 def test_equal_coord():
     hs_filter = 'a == C(100,100)'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"c:100.000000,100.000000"']}}}]
 
 
 def test_equal_NA():
     hs_filter = 'a == NA'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"z:"']}}}]
 
 
 def test_equal_Null():
     hs_filter = 'a == N'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', 'null']}}}]
 
 
 def test_not_equal_Null():
     hs_filter = 'a != N'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$ne': ['$a', 'null']}}}]
 
 
 def test_equal_Marker():
     hs_filter = 'a == M'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"m:"']}}}]
 
 
 def test_equal_uri():
     hs_filter = 'a == `http://l`'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"u:http://l"']}}}]
 
 
 def test_equal_xstr():
     hs_filter = 'a == hex("deadbeef")'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$a', '"x:hex:deadbeef"']}}}]
 
 
 def test_and_ltag_rtag():
     hs_filter = 'site and ref'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$and': [{'$cond': {'if': '$site', 'then': 1, 'else': 0}},
+                                           {'$cond': {'if': '$ref', 'then': 1, 'else': 0}}]}}}]
 
 
 def test_and_andtag_rtag():
     hs_filter = '(site and ref) and his'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match':
+                 {'$expr': {'$and':
+                                [{'$and': [{'$cond': {'if': '$site', 'then': 1, 'else': 0}},
+                                           {'$cond': {'if': '$ref', 'then': 1, 'else': 0}}]},
+                                 {'$cond': {'if': '$his', 'then': 1, 'else': 0}}]}}}]
 
 
 def test_and_ltag_andtag():
     hs_filter = 'his and (site and ref)'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match':
+                 {'$expr': {'$and': [{'$cond': {'if': '$his', 'then': 1, 'else': 0}}, {
+                     '$and': [{'$cond': {'if': '$site', 'then': 1, 'else': 0}},
+                              {'$cond': {'if': '$ref', 'then': 1, 'else': 0}}]}]}}}]
 
 
 def test_and_andtag_andtag():
     hs_filter = '(his and point) and (site and ref)'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match':
+                 {'$expr':
+                      {'$and':
+                           [{'$and': [{'$cond': {'if': '$his', 'then': 1, 'else': 0}},
+                                      {'$cond': {'if': '$point', 'then': 1, 'else': 0}}]}, {
+                                '$and': [{'$cond': {'if': '$site', 'then': 1, 'else': 0}},
+                                         {'$cond': {'if': '$ref', 'then': 1, 'else': 0}}]}]}}}]
 
 
 def test_and_not_ltag_rtag():
     hs_filter = 'not site and not ref'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$and': [{'$not': '$site'}, {'$not': '$ref'}]}}}]
 
 
 def test_and_not_andtag_rtag():
     hs_filter = '(not site and not ref) and not his'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$and': [{'$and': [{'$not': '$site'}, {'$not': '$ref'}]}, {'$not': '$his'}]}}}]
 
 
 def test_and_not_ltag_andtag():
     hs_filter = 'not his and (not site and not ref)'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$and': [{'$not': '$his'}, {'$and': [{'$not': '$site'}, {'$not': '$ref'}]}]}}}]
 
 
 def test_and_not_andtag_andtag():
     hs_filter = '(not his and not point) and (not site and not ref)'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$and': [{'$and': [{'$not': '$his'}, {'$not': '$point'}]},
+                                           {'$and': [{'$not': '$site'}, {'$not': '$ref'}]}]}}}]
 
 
 def test_equal():
     hs_filter = 'geoPostal==78000'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$geoPostal', '"n:78000.000000"']}}}]
 
 
 def test_has_and_equal():
     hs_filter = 'site and geoPostal==78000'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$and': [{'$cond': {'if': '$site', 'then': 1, 'else': 0}},
+                                           {'$eq': ['$geoPostal', '"n:78000.000000"']}]}}}]
 
 
 def test_and_with_not():
     hs_filter = 'site and his and not geoPostal'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$and': [{'$and': [{'$cond': {'if': '$site', 'then': 1, 'else': 0}},
+                                                     {'$cond': {'if': '$his', 'then': 1, 'else': 0}}]},
+                                           {'$not': '$geoPostal'}]}}}]
 
 
 def test_equal_number():
     hs_filter = 'geoPostalCode==1111'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$geoPostalCode', '"n:1111.000000"']}}}]
 
 
 def test_greater_number():
-    hs_filter = 'geoPostalCode > 55400'
+    hs_filter = 'curVal > 1'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match':
+               {'$expr':
+                   {'$gt': [{'$let': {'vars': {
+                       'curVal_': {
+                           '$regexFind':
+                               {'input': '$curVal', 'regex': 'n:([-+]?([0-9]*[.])?[0-9]+([eE][-+]?\\d+)?)'}}},
+                       'in': {'$toDouble': {'$arrayElemAt': ['$$curVal_.captures', 0]}}}},
+                       1.0]}}}]
 
 
 def test_greater_or_equal_number():
     hs_filter = 'geoPostalCode >= 55400'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match':
+               {'$expr':
+                   {'$gte': [{'$let': {'vars': {'geoPostalCode_': {
+                       '$regexFind': {'input': '$geoPostalCode',
+                                      'regex': 'n:([-+]?([0-9]*[.])?[0-9]+([eE][-+]?\\d+)?)'}}},
+                       'in': {'$toDouble': {
+                           '$arrayElemAt': ['$$geoPostalCode_.captures', 0]}}}},
+                       55400.0]}}}]
 
 
 def test_lower_number():
     hs_filter = 'geoPostalCode < 55400'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match':
+               {'$expr':
+                   {'$lt': [{'$let': {'vars': {'geoPostalCode_': {
+                       '$regexFind': {'input': '$geoPostalCode',
+                                      'regex': 'n:([-+]?([0-9]*[.])?[0-9]+([eE][-+]?\\d+)?)'}}},
+                       'in': {'$toDouble': {
+                           '$arrayElemAt': ['$$geoPostalCode_.captures', 0]}}}},
+                       55400.0]}}}]
 
 
 def test_lower_or_equal_number():
     hs_filter = 'geoPostalCode <= 55400'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match':
+               {'$expr':
+                   {'$lte': [{'$let': {'vars': {'geoPostalCode_': {
+                       '$regexFind': {'input': '$geoPostalCode',
+                                      'regex': 'n:([-+]?([0-9]*[.])?[0-9]+([eE][-+]?\\d+)?)'}}},
+                       'in': {'$toDouble': {
+                           '$arrayElemAt': ['$$geoPostalCode_.captures', 0]}}}},
+                       55400.0]}}}]
 
 
 def test_greater_quantity():
     hs_filter = 'temp > 55400°'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match':
+               {'$expr':
+                   {'$gt': [{'$let': {'vars': {
+                       'temp_': {
+                           '$regexFind': {'input': '$temp', 'regex': 'n:([-+]?([0-9]*[.])?[0-9]+([eE][-+]?\\d+)?)'}}},
+                       'in': {'$toDouble': {'$arrayElemAt': ['$$temp_.captures', 0]}}}},
+                       55400.0]}}}]
 
 
 def test_greater_or_equal_quantity():
     hs_filter = 'temp >= 55400°'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match':
+               {'$expr':
+                   {'$gte': [{'$let': {'vars': {
+                       'temp_': {
+                           '$regexFind': {'input': '$temp', 'regex': 'n:([-+]?([0-9]*[.])?[0-9]+([eE][-+]?\\d+)?)'}}},
+                       'in': {'$toDouble': {'$arrayElemAt': ['$$temp_.captures', 0]}}}},
+                       55400.0]}}}]
 
 
 def test_path_equal_quantity():
@@ -325,21 +418,32 @@ def test_and_or():
     hs_filter = '(a or b) and (c or d)'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$and': [
+               {'$or': [{'$cond': {'if': '$a', 'then': 1, 'else': 0}}, {'$cond': {'if': '$b', 'then': 1, 'else': 0}}]},
+               {'$or': [{'$cond': {'if': '$c', 'then': 1, 'else': 0}},
+                        {'$cond': {'if': '$d', 'then': 1, 'else': 0}}]}]}}}]
 
 
 def test_or_and():
     hs_filter = 'site or (elect and point)'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$or': [{'$cond': {'if': '$site', 'then': 1, 'else': 0}}, {
+               '$and': [{'$cond': {'if': '$elect', 'then': 1, 'else': 0}},
+                        {'$cond': {'if': '$point', 'then': 1, 'else': 0}}]}]}}}]
 
 
 def test_and_or_and():
     hs_filter = 'site and (elect or point) and toto'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$and': [{'$and': [{'$cond': {'if': '$site', 'then': 1, 'else': 0}}, {
+               '$or': [{'$cond': {'if': '$elect', 'then': 1, 'else': 0}},
+                       {'$cond': {'if': '$point', 'then': 1, 'else': 0}}]}]},
+                                           {'$cond': {'if': '$toto', 'then': 1, 'else': 0}}]}}}]
 
 
 def test_and_or_path():
@@ -360,11 +464,18 @@ def test_combine_and():
     hs_filter = '(a==1 and b==1) or (c==2 and d==3)'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr':
+                            {'$or':
+                                 [{'$and': [{'$eq': ['$a', '"n:1.000000"']},
+                                            {'$eq': ['$b', '"n:1.000000"']}]},
+                                  {'$and': [{'$eq': ['$c', '"n:2.000000"']},
+                                            {'$eq': ['$d', '"n:3.000000"']}]}]}}}]
 
 
 def test_select_with_id():
     hs_filter = 'id==@p:demo:r:23a44701-3a62fd7a'
     mongo_request = mongo_filter(hs_filter, FAKE_NOW, 1, "customer")
     _check_mongodb(mongo_request)
-    assert False
+    assert mongo_request == \
+           [{'$match': {'$expr': {'$eq': ['$id', '"r:p:demo:r:23a44701-3a62fd7a"']}}}]

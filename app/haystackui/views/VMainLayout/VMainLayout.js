15d8b185669c6f82eb925df3d0d59e6523cae966
Théo Andro <tandro@octo.com>
2021-06-02 14:58:25 +0200

    Add shaystack logo

--- app/haystackui/views/VMainLayout/VMainLayout.js
+++ app/haystackui/views/VMainLayout/VMainLayout.js
@@ -9,7 +9,7 @@ const template = `
             alt="Vuetify Logo"
             class="shrink mr-2"
             contain
-            src="require('../../assets/engieLogo.png')"
+            src="https://raw.githubusercontent.com/engie-group/shaystack/develop/docs/logo.png"
             transition="scale-transition"
             width="90"
             disabled

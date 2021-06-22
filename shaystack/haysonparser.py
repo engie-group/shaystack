a5af372bb3947dc0e27c47e41d577b1b7293e367
theoandro <tandro@octo.com>
2021-06-22 14:07:42 +0200

    WIP add hayson parser

--- shaystack/haysonparser.py
+++ shaystack/haysonparser.py
@@ -31,31 +31,21 @@ from .type import Entity
 from .version import LATEST_VER, Version, VER_3_0
 from .zoneinfo import timezone
 
-URI_META = re.compile(r'([:/?#[\]@&=;"$`])')
+URI_META = 'Uri'
 GRID_SEP = re.compile(r'\n\n+')
 
 # Type regular expressions
-MARKER_STR = 'm:'
-NA_STR = 'z:'
-REMOVE2_STR = 'x:'
-REMOVE3_STR = '-:'
-NUMBER_RE = re.compile(r'^n:(-?\d+(:?\.\d+)?(:?[eE][+\-]?\d+)?)(:? (.*))?$',
-                       flags=re.MULTILINE)
-REF_RE = re.compile(r'^r:([a-zA-Z0-9_:\-.~]+)(:? (.*))?$',
-                    flags=re.MULTILINE)
-DATE_RE = re.compile(r'^d:(\d{4})-(\d{2})-(\d{2})$', flags=re.MULTILINE)
-TIME_RE = re.compile(r'^h:(\d{2}):(\d{2})(:?:(\d{2}(:?\.\d+)?))?$',
-                     flags=re.MULTILINE)
-DATETIME_RE = re.compile(r'^t:(\d{4}-\d{2}-\d{2}T'
-                         r'\d{2}:\d{2}(:?:\d{2}(:?\.\d+)?)'
-                         r'(:?[zZ]|[+\-]\d+:?\d*))(:? ([A-Za-z\-+_0-9]+))?$',
-                         flags=re.MULTILINE)
-URI_RE = re.compile(r'u:(.+)$', flags=re.MULTILINE)
-BIN_RE = re.compile(r'b:(.+)$', flags=re.MULTILINE)
-COORD_RE = re.compile(r'c:(-?\d*\.?\d*),(-?\d*\.?\d*)$',
-                      flags=re.MULTILINE)
-
-STR_ESC_RE = re.compile(r'\\([bfnrt"\\$]|u[0-9a-fA-F]{4})')
+MARKER_STR = 'Marker'
+NA_STR = 'na'
+REMOVE_STR = 'Remove'
+NUMBER_STR = 'Num'
+REF_RE = 'Ref'
+DATE_RE = 'Date'
+TIME_RE = 'Time'
+DATETIME_RE = 'DateTime'
+URI_RE = 'Uri'
+BIN_RE = 'Bin'
+COORD_RE = 'Coord'
 
 
 def _parse_metadata(meta: Entity, version: Version) -> MetadataObject:
@@ -87,132 +77,119 @@ def _parse_row(row: Dict[str, Any], version: Version) -> Entity:
 
 def _parse_embedded_scalar(scalar: Union[None, List, Dict, str],
                            version: Version = LATEST_VER) -> Any:  # pylint: disable=too-many-locals
-    # Simple cases
-    if scalar is None:
-        return None
-    if isinstance(scalar, list):
-        # We support this only in version 3.0 and up.
-        if version < VER_3_0:
-            raise ValueError('Lists are not supported in Haystack version %s'
-                             % version)
-        return list(map(functools.partial(parse_scalar, version=version),
-                        scalar))
     if isinstance(scalar, dict):
-        # We support this only in version 3.0 and up.
-        if version < VER_3_0:
-            raise ValueError('Dicts are not supported in Haystack version %s'
-                             % version)
-        if sys.version_info[0] < 3 and {"meta", "cols", "rows"} <= scalar.viewkeys() \
-                or {"meta", "cols", "rows"} <= scalar.keys():  # Check if grid in grid
-            return parse_grid(scalar)
-        return {k: parse_scalar(v, version=version) for (k, v) in scalar.items()}
-    if scalar == MARKER_STR:
-        return MARKER
-    if scalar == NA_STR:
-        return NA
-    if scalar in (REMOVE2_STR, REMOVE3_STR):
-        # Strictly speaking: x: is a HS 2.0 Remove, and -: is a 3.0 Remove
-        # but we'll treat both the same.
-        return REMOVE
-    if isinstance(scalar, bool):
-        return scalar
-    if scalar == 'n:INF':
-        return float('INF')
-    if scalar == 'n:-INF':
-        return -float('INF')
-    if scalar == 'n:NaN':
-        return float('nan')
-    # Conversion to dict of float value turn them into float
-    # so regex won't work... better just return them
-    if isinstance(scalar, (float, int)):
-        return scalar
+        if isinstance(scalar, list):
+            # We support this only in version 3.0 and up.
+            if version < VER_3_0:
+                raise ValueError('Lists are not supported in Haystack version %s'
+                                 % version)
+            return list(map(functools.partial(parse_scalar, version=version),
+                            scalar))
+        if isinstance(scalar, dict):
+            # We support this only in version 3.0 and up.
+            if version < VER_3_0:
+                raise ValueError('Dicts are not supported in Haystack version %s'
+                                 % version)
+            if sys.version_info[0] < 3 and {"meta", "cols", "rows"} <= scalar.viewkeys() \
+                    or {"meta", "cols", "rows"} <= scalar.keys():  # Check if grid in grid
+                return parse_grid(scalar)
+            return {k: parse_scalar(v, version=version) for (k, v) in scalar.items()}
+        kind = scalar.get('_kind')
+        if kind:
 
-    # Is it a number?
-    match = NUMBER_RE.match(scalar)
-    if match:
-        # We'll get a value and a unit, amongst other tokens.
-        matched = match.groups()
-        value = float(matched[0])
-        if matched[-1] is not None:
-            # It's a quantity
-            return Quantity(value, matched[-1])
-        # It's a raw value
-        return value
+            if kind == MARKER_STR:
+                return MARKER
+            if kind == NA_STR:
+                return NA
+            if kind == REMOVE_STR:
+                return REMOVE
+            if kind == NUMBER_STR:
+                value = scalar.get('val')
+                if value == 'INF':
+                    return float('INF')
+                if value == '-INF':
+                    return -float('INF')
+                if value == 'NaN':
+                    return float('nan')
+                else:
+                    if scalar.get('unit'):
+                        return Quantity(value, scalar.get('unit'))
+            # Conversion to dict of float value turn them into float
+            # so regex won't work... better just return them
+            if isinstance(scalar, (float, int)):
+                return scalar
 
-    # Is it a string?
-    if scalar.startswith('s:'):
-        return scalar[2:]
 
-    # Is it a xstr?
-    if scalar.startswith('x:'):
-        return XStr(*scalar[2:].split(':'))
+            # Is it a xstr?
+            if kind == 'xstr':
+                return XStr(*scalar[2:].split(':'))
 
-    # Is it a reference?
-    match = REF_RE.match(scalar)
-    if match:
-        matched = match.groups()
-        if matched[-1] is not None:
-            return Ref(matched[0], matched[-1])
-        return Ref(matched[0])
+            # Is it a reference?
+            match = REF_RE.match(scalar)
+            if match:
+                matched = match.groups()
+                if matched[-1] is not None:
+                    return Ref(matched[0], matched[-1])
+                return Ref(matched[0])
 
-    # Is it a date?
-    match = DATE_RE.match(scalar)
-    if match:
-        (year, month, day) = match.groups()
-        return datetime.date(year=int(year), month=int(month), day=int(day))
+            # Is it a date?
+            match = DATE_RE.match(scalar)
+            if match:
+                (year, month, day) = match.groups()
+                return datetime.date(year=int(year), month=int(month), day=int(day))
 
-    # Is it a time?
-    match = TIME_RE.match(scalar)
-    if match:
-        (hour, minute, _, second, _) = match.groups()
-        # Convert second to seconds and microseconds
-        if second is None:
-            sec = 0
-            usec = 0
-        elif '.' in second:
-            (whole_sec, frac_sec) = second.split('.', 1)
-            sec = int(whole_sec)
-            usec = int(frac_sec[:6].ljust(6, '0'))
-        else:
-            sec = int(second)
-            usec = 0
-        return datetime.time(hour=int(hour), minute=int(minute),
-                             second=sec, microsecond=usec)
+            # Is it a time?
+            match = TIME_RE.match(scalar)
+            if match:
+                (hour, minute, _, second, _) = match.groups()
+                # Convert second to seconds and microseconds
+                if second is None:
+                    sec = 0
+                    usec = 0
+                elif '.' in second:
+                    (whole_sec, frac_sec) = second.split('.', 1)
+                    sec = int(whole_sec)
+                    usec = int(frac_sec[:6].ljust(6, '0'))
+                else:
+                    sec = int(second)
+                    usec = 0
+                return datetime.time(hour=int(hour), minute=int(minute),
+                                     second=sec, microsecond=usec)
 
-    # Is it a date/time?
-    match = DATETIME_RE.match(scalar)
-    if match:
-        matches = match.groups()
-        # Parse ISO8601 component
-        iso_date = iso8601.parse_date(matches[0])
-        # Parse timezone
-        tzname = matches[-1]
-        if tzname is None:
-            return iso_date  # No timezone given
-        try:
-            time_zone = timezone(tzname)
-            return iso_date.astimezone(time_zone)
-        except TypeError:  # noqa: E722 pragma: no cover
-            # Unlikely code path.
-            return iso_date
+            # Is it a date/time?
+            match = DATETIME_RE.match(scalar)
+            if match:
+                matches = match.groups()
+                # Parse ISO8601 component
+                iso_date = iso8601.parse_date(matches[0])
+                # Parse timezone
+                tzname = matches[-1]
+                if tzname is None:
+                    return iso_date  # No timezone given
+                try:
+                    time_zone = timezone(tzname)
+                    return iso_date.astimezone(time_zone)
+                except TypeError:  # noqa: E722 pragma: no cover
+                    # Unlikely code path.
+                    return iso_date
 
-    # Is it a URI?
-    match = URI_RE.match(scalar)
-    if match:
-        return Uri(match.group(1))
+            # Is it a URI?
+            match = URI_RE.match(scalar)
+            if match:
+                return Uri(match.group(1))
 
-    # Is it a Bin?
-    match = BIN_RE.match(scalar)
-    if match:
-        return Bin(match.group(1))
+            # Is it a Bin?
+            match = BIN_RE.match(scalar)
+            if match:
+                return Bin(match.group(1))
 
-    # Is it a co-ordinate?
-    match = COORD_RE.match(scalar)
-    if match:
-        (lat, lng) = match.groups()
-        return Coordinate(float(lat), float(lng))
+            # Is it a co-ordinate?
+            match = COORD_RE.match(scalar)
+            if match:
+                (lat, lng) = match.groups()
+                return Coordinate(float(lat), float(lng))
+            return scalar
     return scalar
-
 
 def parse_scalar(scalar: Union[str, bool, float, int, list, dict], version: Version = LATEST_VER) -> Any:
     """

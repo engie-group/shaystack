be0cd669ee0feec59785e46b8252e783dde5761b
HW6323 <amine.omri@external.engie.com>
2022-06-16 10:23:58 +0200

    Set random threshold (10-15s) between get_query_execution calls

--- shaystack/providers/athena.py
+++ shaystack/providers/athena.py
@@ -184,7 +184,7 @@ class Provider(DBProvider):
                                                         f'{query_status["StateChangeReason"]}')
                     else:
                         raise Exception(error_message)
-                time.sleep(randint(1, 10))
+                time.sleep(randint(10, 15))
             # getting the csv file that contain query results from s3 output bucket
             reader = self.get_query_results(query_response["QueryExecutionId"])
             return reader

960b68586943b8b954d8f18feaf35abbdabdf39d
HW6323 <amine.omri@external.engie.com>
2022-07-25 10:15:55 +0100

    Fix the REFRESH = 0 bug

--- shaystack/providers/url.py
+++ shaystack/providers/url.py
@@ -566,7 +566,7 @@ class Provider(DBHaystackInterface):  # pylint: disable=too-many-instance-attrib
         minutes_delta = now.minute
         if self._periodic_refresh != 0:
             minutes_delta = (now.minute + self._periodic_refresh) // self._periodic_refresh * self._periodic_refresh
-        next_time = now.replace(minute=0, second=0) + timedelta(minutes=minutes_delta)
+        next_time = now.replace(minute=0) + timedelta(minutes=minutes_delta)
         assert next_time >= now
         if parsed_uri.scheme == "s3":
             assert BOTO3_AVAILABLE, "Use 'pip install boto3'"

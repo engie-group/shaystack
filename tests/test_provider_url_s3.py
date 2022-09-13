c62789b01685d137cabf59a229fe9abb63d30262
HW6323 <amine.omri@external.engie.com>
2022-09-13 17:32:48 +0200

    unit test version filter on TSs

--- tests/test_provider_url_s3.py
+++ tests/test_provider_url_s3.py
@@ -44,9 +44,7 @@ def test_read_last_without_filter(mock_s3, mock_get_url):
     with cast(URLProvider, get_provider("shaystack.providers.url", {})) as provider:
         provider.cache_clear()
         result = provider.read(0, None, None, None, None)
-        print("to be deleted" + str(result.metadata["v"]))
         assert result.metadata["v"] == "3"
-
 
 @patch.object(URLProvider, '_get_url')
 @patch.object(URLProvider, '_s3')
@@ -61,7 +59,6 @@ def test_read_version_without_filter(mock_s3, mock_get_url):
     with get_provider("shaystack.providers.url", {}) as provider:
         version_2 = datetime(2020, 10, 1, 0, 0, 3, 0, tzinfo=None)
         result = provider.read(0, None, None, None, date_version=version_2)
-        print("to be deleted" + str(result.metadata["v"]))
         assert result.metadata["v"] == "3"
 
 
@@ -78,7 +75,6 @@ def test_read_version_with_filter(mock_s3, mock_get_url):
     with get_provider("shaystack.providers.url", {}) as provider:
         version_2 = datetime(2020, 10, 1, 0, 0, 2, 0, tzinfo=None)
         result = provider.read(0, None, None, "id==@id1", version_2)
-        print("to be deleted" + str(result.metadata["v"]))
         assert result.metadata["v"] == "2"
         assert len(result) == 1
         assert result[0]['id'] == Ref("id1")
@@ -130,7 +126,6 @@ def test_lru_version(mock):
 
     with cast(URLProvider, get_provider("shaystack.providers.url", {"REFRESH": 1})) as provider:
         result = provider._download_grid(url, last)
-        print("to be deleted" + str(result.metadata["v"]))
         assert result.metadata["v"] == "3"
 
 
@@ -146,33 +141,32 @@ def test_version(mock, mock_get_url):
     mock_get_url.return_value = "s3://bucket/grid.zinc"
     with get_provider("shaystack.providers.url", {}) as provider:
         versions = provider.versions()
-        print("to be deleted" + str(len(versions)))
         assert len(versions) == 3
 
 
-# @patch.object(URLProvider, '_get_url')
-# @patch.object(URLProvider, '_s3')
-# def test_his_read_with_version_filter(mock_s3, mock_get_url):
-#     """
-#     Args:
-#         mock_s3:
-#         mock_get_url:
-#     """
-#     mock_s3.return_value = _get_mock_s3_updated_ontology()
-#     mock_get_url.return_value = "s3://bucket/grid.zinc"
-#     with cast(URLProvider, haystack_interface.get_provider("shaystack.providers.url", {})) as provider:
-#         provider.cache_clear()
-#         provider._periodic_refresh = 0
-#         version = datetime(2020, 10, 1, 0, 0, 2, 0, tzinfo=None)
-#         result = provider.his_read(entity_id=Ref("id1"),
-#                                    date_version=version,
-#                                    date_range=(
-#                                        datetime.min.replace(tzinfo=pytz.UTC),
-#                                        datetime.max.replace(tzinfo=pytz.UTC)
-#                                    ))
-#         assert (len(result._row)) == 4  # 5 out of 6 since getting all TSs < 2020-10-01T16:30:00
-#         assert result._row[3] == {'ts': datetime(2020, 10, 1, 0, 0, tzinfo=pytz.UTC), 'val': 20.0}
-#
+@patch.object(URLProvider, '_get_url')
+@patch.object(URLProvider, '_s3')
+def test_his_read_with_version_filter(mock_s3, mock_get_url):
+    """
+    Args:
+        mock_s3:
+        mock_get_url:
+    """
+    mock_s3.return_value = _get_mock_s3_updated_ontology()
+    mock_get_url.return_value = "s3://bucket/grid.zinc"
+    with cast(URLProvider, haystack_interface.get_provider("shaystack.providers.url", {})) as provider:
+        provider.cache_clear()
+        provider._periodic_refresh = 0
+        version = datetime(2020, 10, 1, 0, 0, 2, 0, tzinfo=None)
+        result = provider.his_read(entity_id=Ref("id1"),
+                                   date_version=version,
+                                   date_range=(
+                                       datetime.min.replace(tzinfo=pytz.UTC),
+                                       datetime.max.replace(tzinfo=pytz.UTC)
+                                   ))
+        assert (len(result._row)) == 4  # 5 out of 6 since getting all TSs < 2020-10-01T16:30:00
+        assert result._row[3] == {'ts': datetime(2020, 10, 1, 0, 0, tzinfo=pytz.UTC), 'val': 20.0}
+
 #
 # @patch.object(URLProvider, '_get_url')
 # @patch.object(URLProvider, '_s3')
